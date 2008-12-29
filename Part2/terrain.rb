661a6bd8ece7996575f53e7d7fbcac52790808d4
Tyler Church <tylertrain@yahoo.com>
2008-12-28 17:54:08 -0700

    Moved terrain files into part2 folder (to share resources with other lessons), separated the vec3f class from terrain.rb

--- Part2/terrain.rb
+++ Part2/terrain.rb
@@ -125,66 +125,6 @@ class Terrain
 	end
 end
 
-class Vec3f
-	def initialize(x, y, z)
-		@v = []
-		@v[0] = x
-		@v[1] = y
-		@v[2] = z
-	end
-	
-	def [](index)
-		return @v[index]
-	end
-	
-	def *(scale)
-		return Vec3f.new(@v[0] * scale, @v[1] * scale, @v[2] * scale)
-	end
-	
-	def /(scale)
-		return Vec3f.new(@v[0] / scale, @v[1] / scale, @v[2] / scale)
-	end
-	
-	def +(other)
-		return Vec3f.new(@v[0] + other[0], @v[1] + other[1], @v[2] + other[2])
-	end
-	
-	def -(other)
-		return Vec3f.new(@v[0] - other[0], @v[1] - other[1], @v[2] - other[2])
-	end
-	
-	def -@
-		return Vec3f.new(-@v[0], -@v[1], -@v[2])
-	end
-	
-	def magnitude
-		return Math.sqrt((@v[0] ** 2) + (@v[1] ** 2) + (@v[2] ** 2))
-	end
-	
-	def magnitudeSquared
-		return ((@v[0] ** 2) + (@v[1] ** 2) + (@v[2] ** 2))
-	end
-	
-	def normalize
-		m = magnitude
-		return Vec3f.new(@v[0] / m, @v[1] / m, @v[2] / m)
-	end
-	
-	def dot(other)
-		return ((@v[0] * other[0]) + (@v[1] * other[1]) + (@v[2] * other[2]))
-	end
-	
-	def cross(other)
-		return Vec3f.new((@v[1] * other[2]) - (@v[2] * other[1]),
-		(@v[2] * other[0]) - (@v[0] * other[2]),
-		(@v[0] * other[1]) - (@v[1] * other[0]))
-	end
-	
-	def to_s
-		return '(' + @v[0].to_s + ',' + @v[1].to_s + ',' + @v[2].to_s + ')'
-	end
-end
-
 def loadTerrain(filename, height)
 	image = SDL::Surface.loadBMP(filename)
 	t = Terrain.new(image.w, image.h)
