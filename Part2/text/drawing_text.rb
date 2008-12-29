9d4b521555a8c0bc82bd3312ea11b75df9af0967
Tyler Church <tylertrain@yahoo.com>
2008-12-28 21:01:14 -0700

    At this point in time, I don't know what the code translates to, or if what I have so far is correct. I'll come back to it later

--- Part2/text/drawing_text.rb
+++ Part2/text/drawing_text.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'opengl'
 require 'sdl'
 
-PI_TIMES_2_oVER_65536 = 2 * 3.1415926535 / 65536.0
+PI_TIMES_2_OVER_65536 = 2 * 3.1415926535 / 65536.0
 
 class T3DLoadException < Exception
 end
@@ -10,31 +10,32 @@ end
 class T3DFont
 	def initialize(input)
 		buffer = input.read(8)
-	end
-	
-	header = "VTR\0FNT\0"
-	8.times  |i|
-		if buffer[i] != header[i]
-			throw T3DLoadException("Invalid Font File")
+		header = "VTR\0FNT\0"
+		8.times do |i|
+			if buffer[i] != header[i]
+				throw T3DLoadException("Invalid Font File")
+			end
+		end
+		
+		buffer = input.read(5)
+		spaceWidth = buffer.to_f
+		
+		displayListId2d = glGenLists(94)
+		displayListId3d = glGenLists(94)
+		94.times do |i|
+			buffer = input.read(5)
+			scale = buffer.to_f / 65536
+			buffer = input.read(2)
+			width = scale * buffer.to_i
+			buffer = input.read(2)
+			height = scale * buffer.to_i
+			scale /= height
+			widths[i] = width / height
+			buffer = input.read(2)
+			numVerts = buffer.to_i
 		end
 	end
-	
-	buffer = read(5)
-	spaceWidth = buffer.to_f
-	
-	displayListId2d = glGenLists(94)
-	displayListId3d = glGenLists(94)
-	94.times do |i|
-		buffer = read(5)
-		scale = buffer.to_f / 65536
-		buffer = input.read(2)
-		width = scale * buffer.to_i
-		buffer = input.read(2)
-		height - scale * buffer.to_i
-		scale /= height
-		widths[i] = width / height
-		buffer = input.read(2)
-		numVerts = buffer.to_i
-		verts
-	end
 end
+
+b = File.new("charset", "r")
+a = T3DFont.new(b)
