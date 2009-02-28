fe48cf4b690842d7066f152bb10cb78ded71f68b
Tyler Church <tylertrain@yahoo.com>
2009-02-27 19:45:52 -0700

    Started Dev directory for lesson 9

---
+++ Part2/animation/main.cpp
@@ -0,0 +1,200 @@
+/* Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/* File for "Animation" lesson of the OpenGL tutorial on
+ * www.videotutorialsrock.com
+ */
+
+
+
+#include <iostream>
+#include <stdlib.h>
+
+#ifdef __APPLE__
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include "imageloader.h"
+#include "md2model.h"
+
+using namespace std;
+
+const float FLOOR_TEXTURE_SIZE = 15.0f; //The size of each floor "tile"
+
+float _angle = 30.0f;
+MD2Model* _model;
+int _textureId;
+//The forward position of the guy relative to an arbitrary floor "tile"
+float _guyPos = 0;
+
+void cleanup() {
+	delete _model;
+}
+
+void handleKeypress(unsigned char key, int x, int y) {
+	switch (key) {
+		case 27: //Escape key
+			cleanup();
+			exit(0);
+	}
+}
+
+//Makes the image into a texture, and returns the id of the texture
+GLuint loadTexture(Image *image) {
+	GLuint textureId;
+	glGenTextures(1, &textureId);
+	glBindTexture(GL_TEXTURE_2D, textureId);
+	glTexImage2D(GL_TEXTURE_2D,
+				 0,
+				 GL_RGB,
+				 image->width, image->height,
+				 0,
+				 GL_RGB,
+				 GL_UNSIGNED_BYTE,
+				 image->pixels);
+	return textureId;
+}
+
+void initRendering() {
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_COLOR_MATERIAL);
+	glShadeModel(GL_SMOOTH);
+	
+	//Load the model
+	_model = MD2Model::load("tallguy.md2");
+	if (_model != NULL) {
+		_model->setAnimation("run");
+	}
+	
+	//Load the floor texture
+	Image* image = loadBMP("vtr.bmp");
+	_textureId = loadTexture(image);
+	delete image;
+}
+
+void handleResize(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, (float)w / (float)h, 1.0, 200.0);
+}
+
+void drawScene() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
+	glTranslatef(0.0f, 0.0f, -20.0f);
+	glRotatef(-_angle, 0.0f, 1.0f, 0.0f);
+	
+	GLfloat ambientLight[] = {0.3f, 0.3f, 0.3f, 1.0f};
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientLight);
+	
+	GLfloat lightColor[] = {0.7f, 0.7f, 0.7f, 1.0f};
+	GLfloat lightPos[] = {-0.2f, 0.3f, -1, 0.0f};
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+	
+	//Draw the guy
+	if (_model != NULL) {
+		glPushMatrix();
+		glTranslatef(0.0f, 4.5f, 0.0f);
+		glRotatef(-90.0f, 0.0f, 0.0f, 1.0f);
+		glScalef(0.5f, 0.5f, 0.5f);
+		_model->draw();
+		glPopMatrix();
+	}
+	
+	//Draw the floor
+	glTranslatef(0.0f, -5.4f, 0.0f);
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, _textureId);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	
+	glBegin(GL_QUADS);
+	
+	glNormal3f(0.0f, 1.0f, 0.0f);
+	glTexCoord2f(2000 / FLOOR_TEXTURE_SIZE, _guyPos / FLOOR_TEXTURE_SIZE);
+	glVertex3f(-1000.0f, 0.0f, -1000.0f);
+	glTexCoord2f(2000 / FLOOR_TEXTURE_SIZE,
+				 (2000 + _guyPos) / FLOOR_TEXTURE_SIZE);
+	glVertex3f(-1000.0f, 0.0f, 1000.0f);
+	glTexCoord2f(0.0f, (2000 + _guyPos) / FLOOR_TEXTURE_SIZE);
+	glVertex3f(1000.0f, 0.0f, 1000.0f);
+	glTexCoord2f(0.0f, _guyPos / FLOOR_TEXTURE_SIZE);
+	glVertex3f(1000.0f, 0.0f, -1000.0f);
+	
+	glEnd();
+	
+	glutSwapBuffers();
+}
+
+void update(int value) {
+	_angle += 0.7f;
+	if (_angle > 360) {
+		_angle -= 360;
+	}
+	
+	//Advance the animation
+	if (_model != NULL) {
+		_model->advance(0.025f);
+	}
+	
+	//Update _guyPos
+	_guyPos += 0.08f;
+	while (_guyPos > FLOOR_TEXTURE_SIZE) {
+		_guyPos -= FLOOR_TEXTURE_SIZE;
+	}
+	
+	glutPostRedisplay();
+	glutTimerFunc(25, update, 0);
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(400, 400);
+	
+	glutCreateWindow("Animation - videotutorialsrock.com");
+	initRendering();
+	
+	glutDisplayFunc(drawScene);
+	glutKeyboardFunc(handleKeypress);
+	glutReshapeFunc(handleResize);
+	glutTimerFunc(25, update, 0);
+	
+	glutMainLoop();
+	return 0;
+}
+
+
+
+
+
+
+
+
+
