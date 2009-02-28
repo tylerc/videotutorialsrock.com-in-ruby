2fb9967c15083cf9c59eff7f85e21bf8058da05c
Tyler Church <tylertrain@yahoo.com>
2009-02-27 19:35:11 -0700

    Deleted the dev files for lesson 8. Filled out the README for lesson 8.

--- Part2/text/drawing_text.rb
+++ Part2/text/drawing_text.rb
@@ -1,41 +1,116 @@
 require 'rubygems'
 require 'opengl'
 require 'sdl'
+begin
+	require 'text3d'
+rescue LoadError
+	puts 'The load of text3d failed (See README for help)'
+	exit
+end
+include Text3d
 
-PI_TIMES_2_OVER_65536 = 2 * 3.1415926535 / 65536.0
-
-class T3DLoadException < Exception
+def computeScale(strs)
+	maxWidth = 0
+	4.times do |x|
+		width = t3dDrawWidth(strs[x])
+		if width > maxWidth
+			maxWidth = width
+		end
+	end
+	
+	return 2.6 / maxWidth
 end
 
-class T3DFont
-	def initialize(input)
-		buffer = input.read(8)
-		header = "VTR\0FNT\0"
-		8.times do |i|
-			if buffer[i] != header[i]
-				throw T3DLoadException("Invalid Font File")
-			end
-		end
-		
-		buffer = input.read(5)
-		spaceWidth = buffer.to_f
-		
-		displayListId2d = glGenLists(94)
-		displayListId3d = glGenLists(94)
-		94.times do |i|
-			buffer = input.read(5)
-			scale = buffer.to_f / 65536
-			buffer = input.read(2)
-			width = scale * buffer.to_i
-			buffer = input.read(2)
-			height = scale * buffer.to_i
-			scale /= height
-			widths[i] = width / height
-			buffer = input.read(2)
-			numVerts = buffer.to_i
-		end
+@angle = -30.0
+@scale = 1
+STRS = ["Video", "Tutorials", "Rock", ".com"]
+
+def cleanup
+	t3dCleanup
+end
+
+def handleKeypress(key, x, y)
+	case key
+		when 27:
+			cleanup
+			exit
 	end
 end
 
-b = File.new("charset", "r")
-a = T3DFont.new(b)
+def initRendering
+	glEnable(GL_DEPTH_TEST)
+	glEnable(GL_COLOR_MATERIAL)
+	glEnable(GL_LIGHTING)
+	glEnable(GL_LIGHT0)
+	glEnable(GL_NORMALIZE)
+	glShadeModel(GL_SMOOTH)
+	t3dInit
+end
+
+def handleResize(w, h)
+	glViewport(0, 0, w, h)
+	glMatrixMode(GL_PROJECTION)
+	glLoadIdentity
+	gluPerspective(45.0, w / h, 1.0, 200.0)
+end
+
+def drawScene
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	
+	glMatrixMode(GL_MODELVIEW)
+	glLoadIdentity
+	glTranslatef(0.0, 0.0, -8.0)
+	
+	ambientColor = [0.4, 0.4, 0.4, 1.0]
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor)
+	
+	lightColor0 = [0.6, 0.6, 0.6, -1.0]
+	lightPos0 = [-0.5,  0.5, 1.0, 0.0]
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0)
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos0)
+	
+	glRotatef(20.0, 1.0, 0.0, 0.0)
+	glRotatef(-@angle, 0.0, 1.0, 0.0)
+	
+	# draw the strings along the sides of a square
+	glScalef(@scale, @scale, @scale)
+	glColor3f(0.3, 1.0, 0.3)
+	4.times do |x|
+		glPushMatrix
+		glRotatef(90 * x, 0, 1, 0)
+		glTranslatef(0, 0, 1.5 / @scale)
+		t3dDraw3D STRS[x], 0, 0, 0.2
+		glPopMatrix
+	end
+	
+	glutSwapBuffers
+end
+
+def update value
+	@angle += 1.5
+	if @angle > 360
+		@angle -= 360
+	end
+	
+	glutPostRedisplay
+	glutTimerFunc(25, method(:update).to_proc, 0)
+end
+
+if __FILE__ == $0
+	glutInit
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+	glutInitWindowSize(400, 400)
+
+	glutCreateWindow("Drawing Text")
+	initRendering
+
+	@scale = computeScale STRS
+
+	glutDisplayFunc(method(:drawScene).to_proc)
+	glutKeyboardFunc(method(:handleKeypress).to_proc)
+	glutReshapeFunc(method(:handleResize).to_proc)
+	glutTimerFunc(0, method(:update).to_proc, 25)
+
+	glutMainLoop()
+end
+
